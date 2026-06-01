3fcd965ed50d15de755a8b1a6989a855fb20a4a0
EstebanHuincabal <adolfohuincabal@gmail.com>
2026-06-01 13:48:23 -0400

    Areglo del rut de empresas

--- gestion_backend/gestion_backend/asgi.py
+++ gestion_backend/gestion_backend/asgi.py
@@ -1,21 +1,18 @@
 import os
+import django
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "gestion_backend.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gestion_backend.settings")
+
+# django.setup() debe llamarse ANTES de cualquier import de modelos, consumers o
+# rest_framework. get_asgi_application() lo invoca internamente, pero hacerlo
+# explícito garantiza el orden correcto incluso si el import chain cambia.
+django.setup()
 
 from django.core.asgi import get_asgi_application
-
-# Inicializar Django primero
-django_asgi_app = get_asgi_application()
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from g_de_flota import routing
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": URLRouter(
-        routing.websocket_urlpatterns
-    ),
+    "http":      get_asgi_application(),
+    "websocket": URLRouter(routing.websocket_urlpatterns),
 })
